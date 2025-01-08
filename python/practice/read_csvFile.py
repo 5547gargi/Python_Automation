a3c405cdc53372ccbae35dd742e924e3ba566328
Gargi Sarada Das <103807137+5547gargi@users.noreply.github.com>
2025-01-08 13:34:15 +0000

    updatedOnWed Jan  8 13:34:01 UTC 2025

--- python/practice/read_csvFile.py
+++ python/practice/read_csvFile.py
@@ -4,6 +4,13 @@ req_file="/workspaces/Python_Automation/python/practice/newinfo.csv"
 fo=open(req_file,"r")
 # content=csv.reader(fo) # for coma separated csv file *No need to mention: delimiter="," exceptionally 
 content=csv.reader(fo,delimiter="|") # for | separated csv file *Need to mention: delimiter="|" explicitly 
+# print(list(content))
+# print(f'The header is :\n{list(content)[0]}')
+# header=next(content)
+# print("the header is : ",header)
+print("the no of rows are : ",len(list(content))-1)
+'''
 for each in content:
     print(each)
+'''
 fo.close()
