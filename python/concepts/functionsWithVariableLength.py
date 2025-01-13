3bb44c5426bf818bed9a37116ae3ae5696987af7
Gargi Sarada Das <103807137+5547gargi@users.noreply.github.com>
2025-01-13 12:52:23 +0000

    UpdatedOnMon Jan 13 12:52:10 UTC 2025

--- python/concepts/functionsWithVariableLength.py
+++ python/concepts/functionsWithVariableLength.py
@@ -10,6 +10,7 @@ def main():
     return None
 main()
 '''
+'''
 def display(*data):
     for each in data:
         print(type(each))
@@ -21,3 +22,19 @@ def display(*data):
 # display(4,5)
 # display("hello")
 display("Hello",5,4.5)
+'''
+
+# def display(**krg):
+#     print(krg)
+#     return None
+
+# display()
+# display(a=2,b=3)
+# display(x="hi",y=4,z=1.5,user="dev01")
+
+def display(a,**krg):
+    print(a)
+    print(krg)
+    return None
+
+display(22,x="hi",y=4,z=1.5,user="dev01")
