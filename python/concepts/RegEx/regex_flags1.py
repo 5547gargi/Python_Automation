ac7a838d4bdef7a15e5b329873c7689f2c932d57
Gargi Sarada Das <103807137+5547gargi@users.noreply.github.com>
2025-01-16 12:01:43 +0000

    updatedOnThu Jan 16 12:01:29 UTC 2025

---
+++ python/concepts/RegEx/regex_flags1.py
@@ -0,0 +1,11 @@
+import re
+
+text="this is a string ThIs is a new String THIS"
+mypat=r"this"
+# print(re.findall(mypat,text,re.IGNORECASE))
+# print(re.findall(mypat,text,re.I))
+
+text1="""this is a string
+this is second string
+this is third string
+asdf this"""
