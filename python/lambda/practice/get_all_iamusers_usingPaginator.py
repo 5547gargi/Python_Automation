a91ec76e62a9c069b7968ba0d14f1b41e2a28b04
Gargi Sarada Das <103807137+5547gargi@users.noreply.github.com>
2025-03-13 12:15:24 +0000

    updatedOnThu Mar 13 12:13:27 UTC 2025

--- python/lambda/practice/get_all_iamusers_usingPaginator.py
+++ python/lambda/practice/get_all_iamusers_usingPaginator.py
@@ -1,4 +1,5 @@
 import boto3
+
 session=boto3.session.Session(profile_name="dev_root")
 '''
 #using resource object
@@ -7,10 +8,21 @@ cnt=1
 for each_user in iam_res.users.all():
     print(cnt,each_user.user_name)
     cnt=cnt+1
-'''
+
 #using client object
 iam_cli=session.client(service_name="iam", region_name="us-east-1")
 cnt=1
 for each_user in iam_cli.list_users()['Users']:
     print(cnt, each_user['UserName'])
     cnt=cnt+1
+'''
+#using paginator
+iam_cli=session.client(service_name="iam", region_name="us-east-1")
+paginator=iam_cli.get_paginator('list_users')
+#print(paginator.paginate())
+cnt=1
+for each_page in paginator.paginate():
+     #print(len(each_page['Users']))
+     for each_user in each_page['Users']:
+         print(cnt,each_user['UserName'])
+         cnt=cnt+1
