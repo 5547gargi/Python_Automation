433a62321a796ce7cc1ddfc2167d0b880bcf3af6
Gargi Sarada Das <103807137+5547gargi@users.noreply.github.com>
2025-03-19 21:09:15 +0000

    updatedOnWed Mar 19 21:08:57 UTC 2025

--- python/boto3/practice1/creating_iam_user.py
+++ python/boto3/practice1/creating_iam_user.py
@@ -2,6 +2,7 @@ import boto3
 import argparse
 import string
 import secrets
+import sys
 from random import choice
 
 '''
@@ -26,21 +27,34 @@ def random_passsword_generator(length=12):
       return password   
              
        
-def create_iam_user(userName):
+def create_iam_user(userName,password=None,attach_policy=None):
      iam_client=boto3.client('iam')
      try:
          iam_client.get_user(UserName=userName)
          print(f'User {userName} already exists')
      except iam_client.exceptions.NoSuchEntityException:
          try:
+             if password is None:
+                 password=random_passsword_generator()
              iam_client.create_user(UserName=userName)
-             print('User created')
+             iam_client.create_login_profile(UserName=userName, Password=password, PasswordResetRequired=True)
+             print(f'User {userName} is created with password {password}')
+             print(f'User {userName} is created successfully' )
+             if attach_ploicy is not None:
+                 iam_client.attach_user_policy(UserName=userName, PolicyArn="arn:aws:iam:policy/AmazonS3ReadonlyAccess")
+                 print(f'S3ReadOnly Policy is attached to user: {userName}')
          except Exception as e:
              print('Error while creating user {userName} {e}')
 
 if __name__ == '__main__':
       parser=argparse.ArgumentParser(description='Creating IAM user')
       # parser.add_argument('-u','--userName',type=str,required=True)
-      parser.add_argument('--userName',type=str,help='The name of IAM user, owner wants to create',required=True)
+      parser.add_argument('--userName',type=str,help='The name of IAM user, owner wants to create')
+      parser.add_argument('--password', type=str, help='The password of IAM user(default is to generate from random_password_generator)')
+      parser.add_argument('--attach_policy', type=str, help='Attach an IAM policy to the user')
       args=parser.parse_args()
-      create_iam_user(args.userName)
+      if not any(vars(args).values()):
+         parser.print_help()
+         sys.exit(1)
+         
+      create_iam_user(args.userName, password=args.password, attach_policy=args.attach_policy)
